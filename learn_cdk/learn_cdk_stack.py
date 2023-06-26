1fe722594e4040ed149d538da142cd69d2fdd0fe
Atul <atula28os@gmail.com>
2023-06-26 17:31:48 +0530

    updated s3 bucket prop

--- learn_cdk/learn_cdk_stack.py
+++ learn_cdk/learn_cdk_stack.py
@@ -1,9 +1,12 @@
 from aws_cdk import (
 
     Stack,
-    aws_s3 as _s3
+    aws_s3 as _s3, 
+    CfnOutput,
 
 )
+
+
 from constructs import Construct
 from pprint import pprint
 
@@ -13,4 +16,11 @@ class LearnCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        _s3.Bucket(self, 'cdk_bucket_01', bucket_name='atula-demo-01', versioned=True, encryption=_s3.BucketEncryption.KMS_MANAGED)
+        demo01_bucket = _s3.Bucket(self, 'cdk_bucket_01', bucket_name='atula-demo-01', \
+                                   versioned=False, \
+                                   encryption=_s3.BucketEncryption.KMS_MANAGED)
+        
+        demo01_output = CfnOutput(self, 's3-demo01-bucket', 
+                                value=demo01_bucket.bucket_name,
+                                description='atula-demo-01 is my bucket',
+                                export_name='s3-demo01' )
