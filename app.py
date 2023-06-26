b3300ddd04af55f00fd037e31476db6bd39d34e4
Atul <atula28os@gmail.com>
2023-06-26 18:26:43 +0530

    working with two stracks on condition

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import os
 
 import aws_cdk as cdk
 
-from learn_cdk.learn_cdk_stack import LearnCdkStack
-
+from learn_cdk.learn_cdk_stack import LearnCdkStack, MyArtifactBucketStack
 
 app = cdk.App()
-LearnCdkStack(app, "LearnCdkStack",
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+# LearnCdkStack(app, "LearnCdkStack",
 
-    env=cdk.Environment(account='997403957483', region='us-east-1'),
+#     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+#     env=cdk.Environment(account='997403957483', region='us-east-1'),
+#     )
+
+MyArtifactBucketStack(app, "MyDevArtifactBucketStack")
+MyArtifactBucketStack(app, "MyProdArtifactBucketStack", is_prod=True)
 
 app.synth()
--- learn_cdk/learn_cdk_stack.py
+++ learn_cdk/learn_cdk_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
 
     Stack,
     aws_s3 as _s3, 
+    RemovalPolicy,
     CfnOutput,
 
 )
@@ -24,3 +25,15 @@ class LearnCdkStack(Stack):
                                 value=demo01_bucket.bucket_name,
                                 description='atula-demo-01 is my bucket',
                                 export_name='s3-demo01' )
+        
+class MyArtifactBucketStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, is_prod=False, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        if is_prod: 
+            artifact_bucket = _s3.Bucket(self, 'prod_artifact_bucket', bucket_name='atula-demo-02', versioned=True, 
+                                         encryption=_s3.BucketEncryption.S3_MANAGED, 
+                                         removal_policy=RemovalPolicy.RETAIN)
+        else:
+            artifact_bucket = _s3.Bucket(self, 'prod_artifact_bucket', bucket_name='atula-demo-03')
